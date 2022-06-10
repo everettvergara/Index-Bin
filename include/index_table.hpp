e3d111366636f3065c5abc9a470ed1353e08d42c
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-06-10 23:41:19 +0800

    added unsafe optim

--- include/index_table.hpp
+++ include/index_table.hpp
@@ -1,5 +1,5 @@
 /**
- * @file index.hpp
+ * @file index_table.hpp
  * @author Everett Gaius S. Vergara (me@everettgaius.com)
  * @brief A fast but expensive datastruct for hashtables 
  * @version 0.1
@@ -9,17 +9,19 @@
  * 
  */
 
-#ifndef INDEX_HPP
-#define INDEX_HPP
+#ifndef INDEX_TABLE_HPP
+#define INDEX_TABLE_HPP
 
 #include <array>
 
 namespace g80 {
 
+    #define UNSAFE_OPTIM
+
     template<typename uint_type, uint_type N>
-    class index {
+    class index_table {
     public:
-        index() {reset();}
+        index_table() {reset();}
 
         auto reset() -> void {
             bin_ptr_ = {invalid_ptr_};
@@ -27,14 +29,23 @@ namespace g80 {
         }
 
         auto add_to_bin(uint_type ix) -> bool {
+            #ifndef UNSAFE_OPTIM
+            if (ix >= N - 1) return false;
+            #endif
+
             ix_bin_[++bin_ptr_] = ix;
-            bin_loc[ix] = bin_ptr_;
+            bin_loc_[ix] = bin_ptr_;
             return bin_ptr_ != invalid_ptr_;
         }
 
-        auto remove_from_bin(uint_type ix) -> void {
+        auto remove_from_bin(uint_type ix) -> bool {
+            #ifndef UNSAFE_OPTIM
+            if (ix >= N - 1) return false;
+            #endif
+
             std::swap(ix_bin_[bin_loc_[ix]], ix_bin_[bin_ptr_--]);
             bin_loc_[ix] = invalid_ptr_;
+            return true;
         }
 
         inline auto size() -> uint_type {
@@ -47,7 +58,7 @@ namespace g80 {
         // TODO: Add forward iterator for list of available indexes in the bin
 
     private:
-        uint_type invalid_ptr_{~static_cast<uint_type>(0);};
+        const uint_type invalid_ptr_{~static_cast<uint_type>(0)};
         std::array<uint_type, N> ix_bin_;
         std::array<uint_type, N> bin_loc_;
         uint_type bin_ptr_{invalid_ptr_};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-#include "index.hpp"
+#include "index_table.hpp"
 
+using namespace g80;
 auto main(const int argc, const char *argv[]) -> int {
+
+    index_table<uint, 5> table;
+    table.add_to_bin(2);
+    table.add_to_bin(4);
+    table.add_to_bin(6);
+    table.add_to_bin(8);
+
+
+    for (auto &ix : table.get_ix_bin()) {
+        std::cout << ix << "\n";
+    }
+
 
 }
