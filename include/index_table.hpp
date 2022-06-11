857ad91afe76b0af192144e9d4088a34823f4793
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-06-11 09:00:34 +0800

    fixed misc bufs

--- include/index_table.hpp
+++ include/index_table.hpp
@@ -12,87 +12,74 @@
 #ifndef INDEX_TABLE_HPP
 #define INDEX_TABLE_HPP
 
-#include <array>
-
 namespace g80 {
 
     // #define UNSAFE_OPTIM
 
-    template<typename uint_type, uint_type N>
+    template<typename uint_type>
     class index_table {
     
 
     public:
-    
-        index_table() {reset();}
+        // Maximum values that can be stored in index table = 2^sizeof(uint_type) - 1
+        // Index Table uses ~static_cast<uint_type>(0) as invalid ix
 
-        auto reset() -> void {
-            bin_ptr_ = {invalid_ptr_};
-            memset(bin_loc_.data(), ~0, sizeof(uint_type) * N);
+        index_table(uint_type size) : size_(size) {reset(size_);}
+
+        auto reset(uint_type size) -> void {
+
+            if (size != size_) {
+                delete []ix_bin_;
+                delete []bin_loc_;
+                ix_bin_ = new uint_type[size_];
+                bin_loc_ = new uint_type[size_];
+            }
+
+            last_ix_ = INVALID_IX;
+            memset(bin_loc_, ~0, sizeof(uint_type) * size_);
         }
 
-        auto add_to_bin(uint_type ix) -> bool {
+        auto add_to_bin(uint_type ix_to_add) -> bool {
             #ifndef UNSAFE_OPTIM
-            if (ix >= N) return false;
-            if (bin_loc_[ix] != invalid_ptr_) return false;
+            if (ix_to_add >= size_) return false;
+            if (bin_loc_[ix_to_add] != INVALID_IX) return false;
             #endif
 
-            ix_bin_[++bin_ptr_] = ix;
-            bin_loc_[ix] = bin_ptr_;
-            return bin_ptr_ != invalid_ptr_;
+            ix_bin_[++last_ix_] = ix_to_add;
+            bin_loc_[ix_to_add] = last_ix_;
+            return true;
         }
 
         auto remove_from_bin(uint_type ix) -> bool {
             #ifndef UNSAFE_OPTIM
-            if (ix >= N) return false;
-            if (bin_loc_[ix] == invalid_ptr_) return false;
+            if (ix >= size_) return false;
+            if (bin_loc_[ix] == INVALID_IX) return false;
             #endif
 
-            std::swap(ix_bin_[bin_loc_[ix]], ix_bin_[bin_ptr_--]);
-            bin_loc_[ix] = invalid_ptr_;
+            std::swap(ix_bin_[bin_loc_[ix]], ix_bin_[last_ix_--]);
+            bin_loc_[ix] = INVALID_IX;
             return true;
         }
 
         inline auto size() -> uint_type {
-            return bin_ptr_ + 1;
+            return last_ix_ + 1;
         }
 
-        inline auto get_ix_bin() const -> const std::array<uint_type, N> & {return ix_bin_;}
-        inline auto get_bin_loc() const -> const std::array<uint_type, N> & {return bin_loc_;}
-        inline auto get_bin_ptr() const -> uint_type {return bin_ptr_;}
-
+        // inline auto get_ix_bin() const -> const std::array<uint_type, N> & {return ix_bin_;}
+        // inline auto get_bin_loc() const -> const std::array<uint_type, N> & {return bin_loc_;}
+        inline auto get_bin_ptr() const -> uint_type {return last_ix_;}
         
-
-    public:
-
-        struct iterator {
-        private:
-            uint_type *ptr;
-        
-        public:
-            iterator(uint_type *begin) :  ptr = begin {}
-            auto operator*() const -> uint_type & {return *ptr;}
-            auto operator->() -> uint_type * {return ptr;}
-            auto operator++() -> iterator & {++ptr; return *this;}
-            auto operator++(int) -> iterator {iterator t = *this; ++ptr; return t;}
-            friend auto operator==(const iterator &lhs, const iterator &rhs) -> bool {return lhs.ptr == rhs.ptr;}
-            friend auto operator!=(const iterator &lhs, const iterator &rhs) -> bool {return lhs.ptr != rhs.ptr;}
-        };
-
-        auto begin() const -> iterator {return iterator(ix_bin_.data())}
-        auto end() const -> iterator {return iterator(ix_bin_.data() + bin_ptr_ + 1)}
-        auto cbegin() const -> iterator {return iterator(ix_bin_.data())}
-        auto cend() const -> iterator {return iterator(ix_bin_.data() + bin_ptr_ + 1)}
-
-    // Private Variables of index_table
 
     private:
     
-        const uint_type invalid_ptr_{~static_cast<uint_type>(0)};
-        std::array<uint_type, N> ix_bin_;
-        std::array<uint_type, N> bin_loc_;
-        uint_type bin_ptr_{invalid_ptr_};  
+        static constexpr uint_type INVALID_IX {~static_cast<uint_type>(0)};
+        uint_type size_;
+        uint_type *ix_bin_{nullptr}, *bin_loc_{nullptr};
+        uint_type last_ix_{INVALID_IX};  
     };
+
+    // template<typename uint_type>
+    // constexpr uint_type index_table<uint_type>::INVALID_IX {~static_cast<uint_type>(0)};
 }
 
 #endif
