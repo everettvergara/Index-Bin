3dc5b69155b40c482cdcdcfcb9fc09ba1f5d07ec
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-06-11 22:13:35 +0800

    redesign index table and make it index-bin

--- include/index_table.hpp
+++ include/index_table.hpp
@@ -1,5 +1,5 @@
 /**
- * @file index_table.hpp
+ * @file index_bin.hpp
  * @author Everett Gaius S. Vergara (me@everettgaius.com)
  * @brief A fast but expensive datastruct for hashtables 
  * @version 0.1
@@ -23,10 +23,25 @@
  * 
  * @note: 
  * 
- * The maximum values that can be stored in an index_table class is:
- *      {2 ^ no_of_bits(uint_type)} - 1
+ * Index Bin is a datastructure concept wherein you have an N-sized vector
+ * paritioned into used and unused indexes. 
  * 
- * Because index_table uses ~static_cast<uint_type>(0) as its invalid index
+ * Indices under each partition are not in any particular order. But even 
+ * though this is the case, searching for an index will take O(1) because
+ * of the index mapper, which monitors the position of the index under the
+ * bin.
+ * 
+ * Below is a sample data structure of a 10-sized element Index Bin.
+ * 
+ * 
+ * Bin:
+ * Used     | Unused
+ * 0 1 2 3    4 5 6 7 8 9 10
+ * 4 5 2 10 | 9 0 1 3 6 7 8 
+ * 
+ * Index Mapper
+ * 0 1 2 3 4 5 6 7 8  9 10
+ * 5 6 2 7 0 1 8 9 10 4 3  
  * 
  */
 
